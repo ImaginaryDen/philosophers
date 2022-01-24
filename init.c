6142207cf9421035c885ba57823c2dd79400d6a6
ImaginaryDen <denis.marakov@mail.ru>
2022-01-24 16:24:05 +0300

    Основная часть готова

--- init.c
+++ init.c
@@ -34,3 +34,24 @@ int	init_philo(t_info *info)
 	}
 	return (0);
 }
+
+int	init_mutex(t_info *info)
+{
+	int	i;
+
+	i = 0;
+	pthread_mutex_init(&info->message, NULL);
+	info->threads = NULL;
+	info->philos = NULL;
+	info->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * info->num_philo);
+	info->threads = (pthread_t *)malloc(sizeof(pthread_t) * info->num_philo);
+	if (info->threads == NULL || info->forks == NULL)
+		return (1);
+	while (i < info->num_philo)
+	{
+		pthread_mutex_init(info->forks + i, NULL);
+		info->threads[i];
+		i++;
+	}
+	return(0);
+}
---
+++ utils/utils.c
@@ -0,0 +1,55 @@
+#include "philo.h"
+
+void	ft_usleep(int ms)
+{
+	long	time;
+
+	time = get_time();
+	usleep(ms * 920);
+	while (get_time() < time + (long)ms)
+		usleep(ms * 3);
+}
+
+void	free_all(t_info *info)
+{
+	int	i;
+
+	i = 0;
+	pthread_mutex_destroy(&info->message);
+	while (i < info->num_philo)
+	{
+		if (info->threads)
+			pthread_join(info->threads[i], NULL);
+		if (info->forks)
+			pthread_mutex_destroy(info->forks + i);
+		if (info->philos)
+			pthread_mutex_destroy(&info->philos[i].eat);
+		i++;
+	}
+	if (info->forks)
+		free(info->forks);
+	if (info->philos)
+		free(info->philos);
+	if (info->threads)
+		free(info->threads);
+}
+
+unsigned int	get_time(void)
+{
+	struct timeval	current_time;
+
+	gettimeofday(&current_time, NULL);
+	return ((current_time.tv_sec * 1000) + (current_time.tv_usec / 1000));
+}
+
+void	message(t_philo *philo, char *msg)
+{
+	pthread_mutex_lock(&philo->data->message);
+	if (philo->data->live == 0)
+	{
+		pthread_mutex_unlock(&philo->data->message);
+		return ;
+	}
+	printf("%d %d %s\n", get_time() - philo->data->start, philo->num + 1, msg);
+	pthread_mutex_unlock(&philo->data->message);
+}
